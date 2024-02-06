95b410228650ada24886de33b7243491496c3324
Olga <user@enail.com>
2024-02-06 18:04:17 +0100

    Logiczeskie operatory

--- index.js
+++ index.js
@@ -17,3 +17,14 @@ let lastWatchedFilm1 = prompt("What is the last watched film?", ""),
     personalMovieDB.movies[lastWatchedFilm2] = filmEstimation2;
 
     console.log(personalMovieDB);
+
+    //Логические операторы
+
+    console.log(NaN || 2 || undefined);   //2
+    console.log(NaN && 2 && undefined);   // NaN
+    console.log(1 && 2 && 3);   //3
+    console.log(!1 && 2 && !3);  //false
+    console.log(25 || null && !3); //25
+    console.log(NaN || null || !3 || undefined || 5);  //5
+    console.log(NaN || null && !3 && undefined || 5);  //5
+    console.log(5 === 5 && 3 > 1 || 5);  //true
