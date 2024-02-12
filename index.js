60f730e05db0a2d644a468d790be9f1d63921eb4
Olga <user@enail.com>
2024-02-12 21:18:27 +0100

    Homework with functions

--- index.js
+++ index.js
@@ -1,4 +1,28 @@
-let numberOfFilms = prompt("How many films have you watched", "");
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt("How many films have you watched", "");
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt("How many films have you watched", "");
+    }
+}
+start();
+
+function detectPersonalLevel() {
+    if (numberOfFilms < 10) {
+        console.log("Not enough films");
+    } else if (numberOfFilms >= 10 && numberOfFilms <= 30) {
+        console.log("You are a classic watcher");
+    } else if (numberOfFilms > 30) {
+        console.log("You are cineman");
+    } else {
+        console.log("Error!");
+    }
+    
+}
+
+detectPersonalLevel();
+
 
 let personalMovieDB = {
     count : numberOfFilms,
@@ -8,23 +32,43 @@ let personalMovieDB = {
     privat : false
 };
 
-let lastWatchedFilm1 = prompt("What is the last watched film?", ""),
-    filmEstimation1 = prompt("What is your estimation of last watched film?", ""),
-    lastWatchedFilm2 = prompt("What is the last watched film?", ""),
-    filmEstimation2 = prompt("What is your estimation of last watched film?", "");
+// let lastWatchedFilm1 = prompt("What is the last watched film?", ""),
+//     filmEstimation1 = prompt("What is your estimation of last watched film?", ""),
+//     lastWatchedFilm2 = prompt("What is the last watched film?", ""),
+//     filmEstimation2 = prompt("What is your estimation of last watched film?", "");
 
-    personalMovieDB.movies[lastWatchedFilm1] = filmEstimation1;
-    personalMovieDB.movies[lastWatchedFilm2] = filmEstimation2;
+    function rememberMyFilms() {
+        for (i = 0; i < 2; i++) {
+            let lastWatchedFilm1 = prompt("What is the last watched film?", ""),
+                filmEstimation1 = prompt("What is your estimation of last watched film?", "");
+                
+    
+                if (lastWatchedFilm1 != null && filmEstimation1 != null && lastWatchedFilm1 != "" && filmEstimation1 != "" && lastWatchedFilm1.length < 50) {
+                    personalMovieDB.movies[lastWatchedFilm1] = filmEstimation1;
+                    console.log("Done!");
+                } else {
+                    console.log("Error!");
+                    i--;
+                }
+            }
+    }
+
+    rememberMyFilms();
 
     console.log(personalMovieDB);
 
-    //Логические операторы
+    function showMyDB(hidden) {
+        if (!hidden) {
+            console.log(personalMovieDB);
+        }
+    }
+    
+    showMyDB(personalMovieDB.privat);
 
-    console.log(NaN || 2 || undefined);   //2
-    console.log(NaN && 2 && undefined);   // NaN
-    console.log(1 && 2 && 3);   //3
-    console.log(!1 && 2 && !3);  //false
-    console.log(25 || null && !3); //25
-    console.log(NaN || null || !3 || undefined || 5);  //5
-    console.log(NaN || null && !3 && undefined || 5);  //5
-    console.log(5 === 5 && 3 > 1 || 5);  //true
+    function writeYourGenres () {
+        for (let i = 1; i <= 3; i++) {
+        personalMovieDB.genres[i - 1] = prompt(`Your favorite genre by number ${i}`);
+        // personalMovieDB.genres[i - 1] = genre;
+    }
+        }
+   writeYourGenres();
